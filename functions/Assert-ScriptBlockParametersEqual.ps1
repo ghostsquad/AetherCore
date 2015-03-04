17d31a3d100a8f734375b85a6e8b9138415500b7
Weston McNamee <ghost.squadron@gmail.com>
2015-03-03 22:08:24 -0800

    resolves #6

--- functions/Assert-ScriptBlockParametersEqual.ps1
+++ functions/Assert-ScriptBlockParametersEqual.ps1
@@ -7,46 +7,26 @@ function Assert-ScriptBlockParametersEqual {
     )
 
     if($x -eq $null -or $y -eq $null) {
-        throw (New-Object ParametersNotEquivalentException('X or Y is Null'))
+        throw (New-Object [Aether.Core.ParametersNotEquivalentException]('X or Y is Null'))
     }
 
     $xParams = @(?: {$x.Ast.ParamBlock -ne $null} {$x.Ast.ParamBlock.Parameters} {})
     $yParams = @(?: {$y.Ast.ParamBlock -ne $null} {$y.Ast.ParamBlock.Parameters} {})
     if($Strict -and $xParams.Count -ne $yParams.Count) {
-        throw (New-Object ParametersNotEquivalentException(('Param count mismatch. x: {0} y: {1}' -f $xParams.Count, $yParams.Count)))
+        throw (New-Object [Aether.Core.ParametersNotEquivalentException](('Param count mismatch. x: {0} y: {1}' -f $xParams.Count, $yParams.Count)))
     }
 
     [int]$max = ?: { $xParams.Count -le $yParams.Count } { $xParams.Count } { $yParams.Count }
 
     for($i = 0; $i -lt $max; $i++) {
         if($xParams[$i].StaticType -ne $yParams[$i].StaticType) {
-            throw (New-Object ParametersNotEquivalentException(('Param type mismatch. x: {0} y: {1}' -f $xParams[$i].StaticType, $yParams[$i].StaticType)))
+            throw (New-Object [Aether.Core.ParametersNotEquivalentException](('Param type mismatch. x: {0} y: {1}' -f $xParams[$i].StaticType, $yParams[$i].StaticType)))
         }
 
         if($AssertNamesMatch) {
             if($xParams[$i].Name.ToString() -ne $yParams[$i].Name.ToString()) {
-                throw (New-Object ParametersNotEquivalentException(('Param name mismatch. x: {0} y: {1}' -f $xParams[$i].Name, $yParams[$i].Name)))
+                throw (New-Object [Aether.Core.ParametersNotEquivalentException](('Param name mismatch. x: {0} y: {1}' -f $xParams[$i].Name, $yParams[$i].Name)))
             }
         }
     }
-}
-
-if (-not ([System.Management.Automation.PSTypeName]'ParametersNotEquivalentException').Type)
-{
-    Add-Type -WarningAction Ignore -TypeDefinition @"
-    using System;
-    using System.Management.Automation;
-
-    public class ParametersNotEquivalentException : Exception {
-        public ParametersNotEquivalentException(string message)
-            : base(message)
-        {
-        }
-
-        public ParametersNotEquivalentException(string message, Exception inner)
-            : base(message, inner)
-        {
-        }
-    }
-"@
 }
---
+++ src/ParametersNotEquivalentException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Aether.Core
+{
+    public class ParametersNotEquivalentException : Exception
+    {
+        public ParametersNotEquivalentException(string message)
+            : base(message)
+        {
+        }
+
+        public ParametersNotEquivalentException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
